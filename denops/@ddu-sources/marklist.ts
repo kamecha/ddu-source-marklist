05c793f060ab1a9036f516e1fa214f64eb9093e4
kamecha <masakikameyama81@gmail.com>
2024-03-03 18:54:12 +0900

    implement ddu mark source

--- denops/@ddu-sources/marklist.ts
+++ denops/@ddu-sources/marklist.ts
@@ -1,13 +1,39 @@
 import { GatherArguments } from "https://deno.land/x/ddu_vim@v3.10.2/base/source.ts";
+import { fn } from "https://deno.land/x/ddu_vim@v3.10.2/deps.ts";
 import { BaseSource, Item } from "https://deno.land/x/ddu_vim@v3.10.2/types.ts";
+import { ActionData } from "https://deno.land/x/ddu_kind_file@v0.7.1/file.ts";
 
 export type Params = Record<never, never>;
 
 export class Source extends BaseSource<Params> {
-  gather({}: GatherArguments<Params>): ReadableStream<Item<unknown>[]> {
-    throw new Error("Method not implemented.");
+  override kind = "file";
+  override gather(
+    args: GatherArguments<Params>,
+  ): ReadableStream<Item<ActionData>[]> {
+    return new ReadableStream({
+      async start(controller) {
+        const items: Item<ActionData>[] = [];
+        const marklist: fn.MarkInformation[] = await fn.getmarklist(
+          args.denops,
+        );
+        for (const mark of marklist) {
+          items.push({
+            word: mark.mark + " " + mark.file + ":" + mark.pos[1] + ":" +
+              mark.pos[2],
+            action: {
+              bufNr: mark.pos[0],
+              col: mark.pos[2],
+              lineNr: mark.pos[1],
+              path: mark.file,
+            },
+          });
+        }
+        controller.enqueue(items);
+        controller.close();
+      },
+    });
   }
-  params(): Params {
-    throw new Error("Method not implemented.");
+  override params(): Params {
+    return {};
   }
 }
