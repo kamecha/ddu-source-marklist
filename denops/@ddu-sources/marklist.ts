97e827310aacc653296e3aeaa60fcef237a709cf
kamecha <masakikameyama81@gmail.com>
2024-03-04 00:19:31 +0900

    add delete mark action support

--- denops/@ddu-sources/marklist.ts
+++ denops/@ddu-sources/marklist.ts
@@ -1,7 +1,11 @@
 import { GatherArguments } from "https://deno.land/x/ddu_vim@v3.10.2/base/source.ts";
-import { fn } from "https://deno.land/x/ddu_vim@v3.10.2/deps.ts";
+import { Denops, fn } from "https://deno.land/x/ddu_vim@v3.10.2/deps.ts";
 import { BaseSource, Item } from "https://deno.land/x/ddu_vim@v3.10.2/types.ts";
 import { ActionData } from "https://deno.land/x/ddu_kind_file@v0.7.1/file.ts";
+import {
+  ActionFlags,
+  DduItem,
+} from "https://deno.land/x/ddu_vim@v3.6.0/types.ts";
 
 export type Params = {
   buf?: fn.BufNameArg;
@@ -10,6 +14,23 @@ export type Params = {
 
 export class Source extends BaseSource<Params> {
   override kind = "file";
+  override actions = {
+    delete: async (
+      args: { denops: Denops; items: DduItem[] },
+    ): Promise<ActionFlags> => {
+      for (const item of args.items) {
+        const mark = item.data as fn.MarkInformation;
+        const pos: fn.Position = [
+          mark.pos[0],
+          0, // if lnum is 0, delete the mark
+          0, // col is no longer used to delete the mark
+          0,
+        ];
+        await fn.setpos(args.denops, mark.mark, pos);
+      }
+      return ActionFlags.RefreshItems;
+    },
+  };
   override gather(
     args: GatherArguments<Params>,
   ): ReadableStream<Item<ActionData>[]> {
@@ -35,6 +56,7 @@ export class Source extends BaseSource<Params> {
               lineNr: mark.pos[1],
               path: path,
             },
+            data: mark,
             highlights: [
               {
                 name: "ddu-source-marklist-header",
