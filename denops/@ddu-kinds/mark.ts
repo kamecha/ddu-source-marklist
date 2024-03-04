3a185abf0dbdd211c586f9794e0851fddbc3e463
kamecha <masakikameyama81@gmail.com>
2024-03-04 17:31:49 +0900

    fix

--- denops/@ddu-kinds/mark.ts
+++ denops/@ddu-kinds/mark.ts
@@ -3,7 +3,7 @@ import { BaseKind } from "https://deno.land/x/ddu_vim@v3.10.2/base/kind.ts";
 export type Params = Record<never, never>;
 
 export class Kind extends BaseKind<Params> {
-  params(): Params {
-    throw new Error("Method not implemented.");
+  override params(): Params {
+    return {};
   }
 }
